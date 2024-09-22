302101c2cb8fa62854c6a125655993465835e375
sebastianjanborowski <166649413+sebastianjanborowski@users.noreply.github.com>
2024-09-22 12:49:17 +0200

    Add files via upload

---
+++ wordpresMotyw/template-parts/content-kontakt.php
@@ -0,0 +1,20 @@
+<section class="container-fluid fotoBg">
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="headerContent">
+                Skontaktuj się z nami
+            </div>
+            <div class="form">
+                Imię i nazwisko: <br/>
+                <input class="input" type="text" id="imieNazwisko" required><br/>
+                
+                E-mail: <br/>
+                <input class="input" type="email" id="email" required><br/>
+                
+                Treść wiadomości: <br/>
+                <textarea class="input2" id="trescWiadomosci" required></textarea><br/>
+                <button onclick="formInputs()" class="button" id="button" type="submit">Wyślij</button>
+            </div>
+        </div>
+    </div>
+</section>
---
+++ wordpresMotyw/template-parts/content-main.php
@@ -0,0 +1,191 @@
+<section class="container-fluid bgColor" id="homeFinish">
+    <div class="row">
+        <div class="col-lg-2 displayN">
+            <img class="imagesR" src="<?php echo get_template_directory_uri(); ?>/assets/images/kwadraty.png">
+        </div>
+        <div class="col-lg-4 col-sm-12">
+        <div class="headerContentMain">
+               <p class="deg"><span class="spanP">Studia po angielsku w AEH</span></p>
+            </div>
+            <h5 class="bold">Otwórz sobie drzwi​</h5>
+            <h5 class="bold">do międzynarodowej kariery</h5>
+            <div class="mainText">
+                Czy wiesz, że studia po angielsku w Akademii Ekonomiczno-Humanistycznej mogą znacząco zmienić Twoją przyszłość zawodową? <span class="span">Wybierając anglojęzyczny program studiów w AEH, zyskasz nie tylko przewagę w późniejszych procesach rekrutacyjnych, ale też umiejętności komunikacyjne umożliwiające pracę w międzynarodowych firmach oraz łatwiejszy dostęp do rynków pracy na całym świecie. ​</span>
+            </div>
+        </div>
+    </div>
+</section>
+<div class="col-lg-6 squere">
+    <img id="myImage" src="<?php echo get_template_directory_uri(); ?>/assets/images/photo1.png">
+
+    <div class="uklad">
+        <input id="jeden" class="inputMain jeden" value="" type="submit" onclick="removePhotoOne()">
+        <input id="dwa" class="inputMain dwa" value="" type="submit" onclick="removePhotoTwo()">
+        <input id="trzy" class="inputMain trzy" value="" type="submit" onclick="removePhotoThree()">
+        <input id="cztery" class="inputMain cztery" value="" type="submit" onclick="removePhotoFour()">
+    </div>
+    
+</div>
+
+
+<section class="container-fluid " id="dlaczegoFinish">
+    <div class="headerContentMain2">
+        <p class="deg"><span class="spanP">Studiowanie po angielsku w Polsce – dlaczego warto?​</span></p>
+    </div>
+    <div class="row">
+        <div class="col-lg-3 height odstep marginMobile">
+        <img class="myImage2" src="<?php echo get_template_directory_uri(); ?>/assets/images/photo5.png">
+           <div class="MainText height2">
+                <br/>Zyskanie biegłości językowej​Studiując w języku angielskim, pogłębiasz nie tylko wiedzę z obszaru wybranego kierunku, ale też <b>szlifujesz język angielski we wszystkich sprawnościach lingwistycznych</b> (mówienie, rozumienie, pisanie, czytanie) i poznajesz słownictwo branżowe.​
+           </div> 
+
+        </div>
+        <div class="col-lg-3 height rownanie marginMobile2">
+        <img class="myImage2" src="<?php echo get_template_directory_uri(); ?>/assets/images/photo6.png">
+           <div class="MainText height3">
+                <br/>Edukacja akademicka w języku angielskim <b>pomoże Ci wyrobić w sobie nawyk „myślenia po angielsku”,</b> co z kolei ułatwi Ci komunikację i rozwój w środowisku zagranicznym oraz międzynarodowym.​
+           </div> 
+        </div>
+
+        <div class="col-lg-3 height odstep2 marginMobile2">
+        <img class="myImage2" src="<?php echo get_template_directory_uri(); ?>/assets/images/photo7.png">
+           <div class="MainText height4">
+               <br/><b>​Absolwenci studiów anglojęzycznych są bardziej konkurencyjni na rynku pracy,</b> co zwiększa ich szanse na lepsze stanowiska, wyższe wynagrodzenie i rozwój w strukturach firm o zasięgu globalnym.​
+           </div> 
+        </div>
+        <div class="col-lg-3 height marginMobile2">
+        <img class="myImage2" src="<?php echo get_template_directory_uri(); ?>/assets/images/photo8.png">
+           <div class="MainText height5">
+                <br/>Studia realizowane po angielsku zwiększają mobilność zawodową – <b>zyskasz możliwość poruszania się między krajami i rynkami pracy, a także znajomość międzynarodowego języka biznesu. ​</b>
+           </div> 
+        </div>
+    </div>
+</section>
+
+
+<section class="container-fluid odstep5" id="zapisacFinish">
+    <div class="row">
+        <div class="col-lg-12">
+            <p class="headerP"><span class="headerSpan">Rozpocznij studia w języku angielskim w AEH​ w trzech prostych krokach:</span>​</p>
+        </div>
+
+        <div class="col-lg-4 odstep3 odstep4">
+                <h1 class="headerNumber">1<span class="kropka">.</span></h1>
+                <h1 class="headerZnakOne">></h1>
+                <h1 class="headerZnakTwo">></h1>
+            <div class="mainContentB height22">
+                <b class="b">Zastanawiasz się, czy Twoja znajomość angielskiego jest wystarczająca do podjęcia studiów w tym języku?</b>
+
+                <p class="mainContentP">
+                    Tak – jeśli jako kandydat na studia posługujesz się angielskim na poziomie min. <b>B2 (wyższym średniozaawansowanym)</b>. Aktywne studiowanie w naszym międzynarodowym środowisku akademickim <b>pozwoli Ci rozwinąć biegłość językową szybciej, niż myślisz!​</b>
+                </p>
+            </div>
+        </div>
+
+        <div class="col-lg-4 odstep3">
+                <h1 class="headerNumber">2<span class="kropka">.</span></h1>
+                <h1 class="headerZnakOne">></h1>
+                <h1 class="headerZnakTwo">></h1>
+            <div class="mainContentB height33">
+                <b class="b">Kształcenie po angielsku dla obywateli polskich oferujemy na większości kierunków studiów licencjackich i magisterskich z naszej OFERTY.</b>
+
+                <p class="mainContentP">
+                    Informację o tym, czy dany kierunek prowadzony jest po angielsku (wraz z cennikiem studiów anglojęzycznych) znajdziesz w opisie poszczególnych programów.​                </p>
+            </div>
+        </div>
+        
+        <div class="col-lg-4 odstep55">
+                <h1 class="headerNumber1 rownanie2">3<span class="kropka">.</span></h1>
+            <div class="mainContentB height44">
+                <b class="b">
+                    Znalazłeś ofertę studiów po angielsku w AEH, które Cię interesują?​
+                </b>
+
+                <p class="mainContentP">
+                    Zapisz się na studia anglojęzyczne, postępując zgodnie z ogólnymi ZASADAMI REKRUTACJI. Podczas wypełniania formularza rejestracyjnego na naszej stronie internetowej, w pierwszym kroku zaznacz język studiów: angielski i podążaj za dalszymi instrukcjami.​                </p>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section class="container-fluid" id="faqFinish">
+    <div class="row">
+        <div class="col-lg-10">
+            <p class="headerP2"><span class="headerSpan">Studia po angielsku w AEH – pytania i odpowiedzi​</span>​</p>
+            <p class="headerPB">Chcesz podjąć studia po angielsku w Akademii Ekonomiczno-Humanistycznej?</p>
+            <p class="headerP3">Poniżej znajdziesz odpowiedzi na najczęściej zadawane pytania kandydatów.</p>
+        </div>
+
+        <div class="col-lg-2">
+             <img class="imagesR2" src="<?php echo get_template_directory_uri(); ?>/assets/images/kwadraty2.svg">
+        </div>
+
+        <div class="col-lg-3">
+            
+                <img class="imagesR4" src="<?php echo get_template_directory_uri(); ?>/assets/images/kwadraty4.svg">
+                <div class="underLine">              
+                    <p class="faq">FAQ:</p>
+                </div>
+            
+            <img class="imagesR3" src="<?php echo get_template_directory_uri(); ?>/assets/images/kwadraty3.svg">
+        </div>
+
+        <div class="col-lg-6 col-md-12 positionAbsolutOne">
+           <div class="optionOne movable-element">
+            <button class="toggle-button position" onclick="toggleBlock('block1', this)">
+                <div class="line horizontal"></div>
+                <div class="line vertical"></div>
+            </button>
+                <p class="optionOneP">Czy są jakieś różnice w programach studiów polsko- i anglojęzycznych na tym samym kierunku?</p>   
+              <div class="block block1">
+                    <p class="showOptionOneP">Nie ma - programy róźnią się jedynie językiem wykładowym, w którym prowadzone są zajęcia.</p>
+              </div>
+
+            <button class="toggle-button position2" onclick="toggleBlock('block2', this)">
+                <div class="line horizontal"></div>
+                <div class="line vertical"></div>
+            </button>
+                <p class="optionTwoP">Czy koszty studiów anglojęzycznych różnią się od analogicznych studiów polskich?</p>   
+              <div class="block block2">
+                <p class="showOptionTwoP">Tak - cennik studiów anglojęzycznych dla studentów z obywatelstwem polskim zjadziesz w opisie programu,który przewiduje taką opcję,w sekcji:Studiuj ten kierunek w języku angielskim.</p>
+              </div>
+              
+
+              <button class="toggle-button position3" onclick="toggleBlock('block3', this)">
+                <div class="line horizontal"></div>
+                <div class="line vertical"></div>
+            </button>
+                <p class="optionThreP">Czy wszystkie kierunki z oferty uczelni można realizować w języku angielskim?</p>   
+              <div class="block block3">
+                <p class="showOptionThreP">Możliwość wyboru języka studiow - polski lub angielski - dotyczy większości kierunków z naszej oferty.Prowadzimy też studia realizowane wyłącznie w języku polskim,jak i dostępne tylko po angielsku - co wynika ze specifiki danego kierunku.Informacje na ten języka studiów znajdziesz zawsze w opisie konkretnego programu.</p>
+              </div>
+
+
+              <button class="toggle-button position4" onclick="toggleBlock('block4', this)">
+                <div class="line horizontal"></div>
+                <div class="line vertical"></div>
+            </button>
+                <p class="optionFourP">Czy proces rekrutacji na studia realizowane w języku angielskim różni się zapisu na studia polskojęzyczne?</p>   
+              <div class="block block4">
+                <p class="showOptionFourP">Nie - w obu przypadkach zapisu możesz dokonać poprzez naszą stronę internetową,wypełniając internetowy formularz rejestracyjny,który przewiduje opcje wyboru języka studiow,a następnie realizując dalsze kroki PROCESU REKRUTACJI.</p>
+              </div>
+
+           </div>
+           
+             
+        </div>
+
+        
+
+        
+
+    </div>
+    <p id="kontaktFinish" class="lastHeader">Jesteś zainteresowany?</p>
+        <a class="lastInput" href="<?php echo get_permalink( get_page_by_path('kontakt') ); ?>">Skontaktuj się poprzez formularz</a>
+
+</section>
+
+<script type="text/javascript">
+    var themeUrl = "<?php echo esc_url(get_stylesheet_directory_uri()); ?>";
+</script>
